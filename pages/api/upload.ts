af250b319946e133b8dfce22f53adc13e93020bb
Tsuyoshi Yoshioka <tsuyogoro@gmail.com>
2023-08-19 09:54:19 +0900

    Upload images

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,75 +1,82 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import multer from 'multer';
-import { Storage } from '@google-cloud/storage';
 
-const storage = new Storage({
-  projectId: 'your-project-id',
-  keyFilename: 'path/to/serviceAccountKey.json',
-});
-const bucket = storage.bucket('your-bucket-name');
+import formidable from 'formidable';
+import { createWriteStream } from 'fs';
 
-const config = {
+const UPLOADED_FOLDER = './uploads';
+
+export const config = {
   api: {
     bodyParser: false,
   },
 };
 
-const upload = multer({ dest: 'uploads/' });
-
-// const router = createRouter<NextApiRequest, NextApiResponse>();
-// router.use(uploadMiddleWare);
-// router.post((req, res) => {});
-
-// export default router;
-
-const uploadHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('aaaaa');
-
-  if (req.method !== 'POST') {
-    res.status(405).send('Method Not Allowed');
-    return;
-  }
-
-  // https://betterprogramming.pub/upload-files-to-next-js-with-api-routes-839ce9f28430
-  // https://serip39.hatenablog.com/entry/2020/08/27/083000
-
-  // https://zenn.dev/niccari/articles/f95c0a31ed40a1
-
-  console.log('content-type');
-  console.log(req.headers['content-type']);
-  console.log('req.body');
-  console.log(req.body);
-
-  upload.single('image');
-
-  // try {
-
-  // await upload.single('image')(req, res, () => {});
-  //   const file = req.file;
-
-  //   if (!file) {
-  //     res.status(400).send('アップロードされた画像がありません');
-  //     return;
-  //   }
-
-  //   const blob = bucket.file(file.originalname);
-  //   const blobStream = blob.createWriteStream();
-
-  //   blobStream.on('error', (error) => {
-  //     console.error('エラー:', error);
-  //     res.status(500).send('画像のアップロード中にエラーが発生しました');
-  //   });
-
-  //   blobStream.on('finish', () => {
-  //     console.log('画像がFirebase Storageにアップロードされました');
-  //     res.send(200);
-  //   });
-
-  //   blobStream.end(file.buffer);
-  // } catch (error) {
-  //   console.error('エラー:', error);
-  //   res.status(500).send('サーバーエラーが発生しました');
-  // }
+type Data = {
+  name: string;
 };
 
-// export default uploadHandler;
+// https://zenn.dev/niccari/articles/f95c0a31ed40a1
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data | undefined>) => {
+  if (req.method !== 'POST') {
+    return;
+  }
+  if (!req.headers['content-type']?.includes('multipart/form-data')) {
+    res.status(400).send(undefined);
+    return;
+  }
+  const form = formidable({ multiples: true, uploadDir: __dirname });
+
+  // Memo:
+  // form.parse(req) を呼ぶと、順々にcallbackが呼ばれる模様
+  // callbackを設定 -> parseという流れでファイルのhandlingをするのが良さげ?
+  // https://qiita.com/dimyasvariant/items/2dc9872684f487f8324d
+  form.onPart = (part) => {
+    if (part.originalFilename === '' || !part.mimetype) {
+      form._handlePart(part);
+    } else if (part.originalFilename) {
+      const originalFileName = part.originalFilename.startsWith('.')
+        ? part.originalFilename.slice(0)
+        : part.originalFilename;
+      const fileName = `${new Date().getTime()}-${originalFileName}`;
+      const stream = createWriteStream(`${UPLOADED_FOLDER}/${fileName}`);
+
+      part.pipe(stream);
+
+      part.on('end', () => {
+        console.log(part.originalFilename + ' is uploaded');
+        stream.close();
+
+        res.status(200).send({
+          name: fileName,
+        });
+      });
+    }
+  };
+
+  form.once('error', (e) => {
+    res.status(500).send(e);
+  });
+
+  form.parse(req, async (err, fields, files) => {
+    console.log('Call back of form.parse');
+    // if (!Object.hasOwn(files, 'image')) {
+    //   throw new Error('imageFile not specified.');
+    // }
+    // if (err) {
+    //   res.statusCode = 500;
+    //   res.end();
+    //   return;
+    // }
+    // const images = files['image'];
+    // const imageFile = Array.isArray(images) ? images[0] : images;
+
+    // console.log('file size - ' + imageFile.size);
+    // // ファイルをなんやかんやする
+
+    // res.status(200).send({
+    //   name: 'Uploaded file size - ' + imageFile.size,
+    // });
+  });
+};
+
+export default handler;
